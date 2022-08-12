15e8c4aa9a70e827ba15902b9c08530d911403c7
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-08-12 13:54:48 +1000

    updated JS

--- Develop/server.js
+++ Develop/server.js
@@ -4,12 +4,17 @@ const db= require('./db/db.json');
 const fs=require ('fs');
 const { v4: uuidv4 } = require('uuid');
 const app=express();
-const PORT= process.env.PORT || 8080;
+const PORT= process.env.PORT || 3000;
 app.use(express.static('public'));
 
 //Middleware for Parsign JSON and urlencoded
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+app.get('/api/notes', (req, res) => {
+    console.log('hitting api/notes route GET request')
+    res.json(db)
+})
 
 app.get('/notes', (req,res) => {
     console.log('notes GET request file');
@@ -18,17 +23,23 @@ app.get('/notes', (req,res) => {
 )
 
 
-app.post('/notes', (req, res) => {
+app.post('/api/notes', (req, res) => {
     console.log('hitting api/notes route POST request')
 
-    let newNote = req.body;
-    newNote.id = uuidv4();
+    let addNote = req.body;
+    addNote.id = uuidv4();
     db.push(newNote)
     fs.writeFileSync('./db/db.json', JSON.stringify(db), (err) => {
         if (err) throw err;
     });
     res.send(db);
 })
+
+app.delete(`/api/notes/:id`, (req, res) => {
+    console.log("DELETE Request Called for /api endpoint")
+    res.send("DELETE Request Called")
+ })
+
 
 app.get('*',(req,res)=>{
     console.log('notes * GET request file');
